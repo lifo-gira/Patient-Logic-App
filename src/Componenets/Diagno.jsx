926c9b1ab6450dd2abb9721e19c051a4ccf87408
Inbasekar <inbatech2024@gmail.com>
2024-03-09 17:42:29 +0530

    Added tilting effect in cards in diagno page

--- src/Componenets/Diagno.jsx
+++ src/Componenets/Diagno.jsx
@@ -32,6 +32,7 @@ import Profilebar from "./Profilebar";
 import Progresstimeline from "./Progresstimeline";
 import Maingraph from "./Maingraph";
 import Maingraphinfo from "./Maingraphinfo";
+import { Tilt } from 'react-tilt'
 
 import {
   Drawer,
@@ -1461,6 +1462,19 @@ const Diagno = () => {
     setRedLineGraphData(PlotArray);
   };
 
+
+  const defaultOptions = {
+    reverse:        false,  // reverse the tilt direction
+    max:            5,     // max tilt rotation (degrees)
+    perspective:    1000,   // Transform perspective, the lower the more extreme the tilt gets.
+    scale:          1.1,    // 2 = 200%, 1.5 = 150%, etc..
+    speed:          1000,   // Speed of the enter/exit transition
+    transition:     true,   // Set a transition on enter/exit.
+    axis:           null,   // What axis should be disabled. Can be X or Y.
+    reset:          true,    // If the tilt effect has to be reset on exit.
+    easing:         "cubic-bezier(.03,.98,.52,.99)",    // Easing on enter/exit.
+  }
+
   const generateCards = () => {
     const cards = [];
     const endIndex = Math.min(startIndex + cardsPerPage, totalCards);
@@ -1470,6 +1484,7 @@ const Diagno = () => {
 
       cards.push(
         <div key={i} className="card" onClick={() => handleCardClick(i)}>
+        <Tilt options={defaultOptions}>
           <Card
             key={i}
             
@@ -1509,6 +1524,7 @@ const Diagno = () => {
               </ul>
             </CardBody>
           </Card>
+          </Tilt>
         </div>
       );
     }
