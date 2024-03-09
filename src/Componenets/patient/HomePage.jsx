7361db5b3a21415041727b222c84c4e08a576040
AnirudhPMenon <anirudh20017@gmail.com>
2024-03-09 16:42:09 +0530

    wo4 done

--- src/Componenets/patient/HomePage.jsx
+++ src/Componenets/patient/HomePage.jsx
@@ -475,30 +475,17 @@ const HomePage = () => {
               </div>
             </div>
             <div className="w-full h-full">
-              <ResponsiveContainer width="100%" height="100%">
-              <AreaChart
-          width={500}
-          height={400}
-          data={romData}
-          margin={{
-            top: 10,
-            right: 30,
-            left: 0,
-            bottom: 0,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <defs>
-            <linearGradient id="splitColor" x1="0" y1="0" x2="0" y2="1">
-              <stop offset={off} stopColor="green" stopOpacity={1} />
-              <stop offset={off} stopColor="red" stopOpacity={1} />
-            </linearGradient>
-          </defs>
-          <Area type="monotone" dataKey="rom" stroke="#000" fill="url(#splitColor)" />
-        </AreaChart>
+            <ResponsiveContainer width="100%" height="100%">
+                <BarChart
+                  data={romData}
+                  layout="vertical"
+                  margin={{ top: 20, right: 30, left: 50, bottom: 5 }}
+                >
+                  <XAxis type="number" />
+                  <YAxis dataKey="name" type="category" />
+                  <Tooltip />
+                  <Bar dataKey="rom" fill="#4F46E5" shape={<CustomBar />} />
+                </BarChart>
               </ResponsiveContainer>
             </div>
           </Card>
